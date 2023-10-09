cf358e2e09699e8ab4b90559312670c6c9ae2b85
Yesod <Yesod@DESKTOP-G4B5NF0>
2023-10-09 12:26:49 -0300

    NavBar mobile

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,13 +1,14 @@
 import Nav from '../Nav/Nav';
 import React from 'react';
 import './Header.css';
+import logo from '../Components/img/LogoGabriel.png';
 
 class Header extends React.Component {
   render() {
     return(
     <div className='header-armazem'>
       <div className="Header">
-        Header
+        <img src={logo} alt="Logo" id='logo'/>
       </div>
       <Nav/>
     </div>
--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Nav.css';
 
-class Nav extends React.Component {
+const Nav = () => {
+  const [burger_class, setBurgerClass] = useState("burger-bar unclicked")
+  const [menu_class, setMenuClass] = useState("menu hidden")
+  const [isMenuClicked, setIsMenuClicked] = useState(false)
 
-  render() {
+  const updateMenu = () => {
+    if(!isMenuClicked) {
+        setBurgerClass("burger-bar clicked")
+        setMenuClass("menu visible")
+    }
+    else {
+        setBurgerClass("burger-bar unclicked")
+        setMenuClass("menu hidden")
+    }
+    setIsMenuClicked(!isMenuClicked)
+  }
+
     return(
     <div className="Nav">
       <ul className='Url-Nav'>
@@ -12,9 +26,25 @@ class Nav extends React.Component {
         <li><a href='#'>ABOUT</a></li>
         <li><a href='#'>CONTACT</a></li>
       </ul>
+      <div className='Url-Nav-Mob' style={{width: '100%',height: '5em'}}>
+              <div className="burger-menu" onClick={updateMenu}>
+                  <div className={burger_class} ></div>
+                  <div className={burger_class} ></div>
+                  <div className={burger_class} ></div>
+              </div>
+
+          <div className={menu_class}>
+          <ul>
+            <li><a href='#'>HOME</a></li>
+            <li><a href='#'>PROJECTS</a></li>
+            <li><a href='#'>ABOUT</a></li>
+            <li><a href='#'>CONTACT</a></li>
+          </ul>
+          </div>
+      </div>
     </div>
     )
-  }
+  
 }
 
 export default Nav;
