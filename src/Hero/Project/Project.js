a09b567c4ad3328e00258a3dfd567efced765fc5
Gabriel Barros de Almeida <GabrielAlmeidaCI@hotmail.com>
2024-02-03 00:47:22 -0300

    update-img

--- src/Hero/Project/Project.js
+++ src/Hero/Project/Project.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {register} from 'swiper/element/bundle'
 import {Swiper, SwiperSlide} from 'swiper/react'
 import 'swiper/css';
@@ -6,19 +6,41 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 import './Project.css';
+import {
+  imgCrew,
+  imgCrewMob,
+  imgApiPoke,
+  imgRpgBot,
+  imgDroneLib,
+  imgQlippoth,
+} from '../../Components/imgProjects';
+
 register();
 
 
 
 
 function Project(){
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+  
   const data = [
-    {id: '1', image: 'https://sujeitoprogramador.com/wp-content/uploads/2022/08/fullstack-blog.png'
-    ,link:'https://sujeitoprogramador.com/wp-content/uploads/2022/08/fullstack-blog.png' },
-    {id: '2', image: 'https://sujeitoprogramador.com/wp-content/uploads/2022/08/fullstack-blog.png'
-    ,link:'https://sujeitoprogramador.com/wp-content/uploads/2022/08/fullstack-blog.png'},
-    {id: '3', image: 'https://sujeitoprogramador.com/wp-content/uploads/2022/08/fullstack-blog.png'
-    ,link:'https://sujeitoprogramador.com/wp-content/uploads/2022/08/fullstack-blog.png'}
+    {id: '1', image: windowWidth < 768 ? imgCrew : imgCrewMob, link: 'https://github.com/Yesod-star/crew-app'},
+    {id: '2', image: imgApiPoke, link:'https://github.com/Yesod-star/ApiPokemonCSharp'},
+    {id: '3', image: imgRpgBot, link:'https://github.com/Yesod-star/RpgUltimateBot'},
+    {id: '4', image: imgDroneLib, link:'https://github.com/Yesod-star/CSharpDroneLib'},
+    {id: '5', image: imgQlippoth, link:'https://github.com/Yesod-star/Qlippoth'}
   ]
     return(
     <div className="Project">
